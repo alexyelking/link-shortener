5f8c2b27e03deea5fd83c6c40c36083cabd2b0a5
Alexander <mistodrake@gmail.com>
2023-05-24 23:26:56 +0600

    GetList refactor

--- app/index.php
+++ app/index.php
@@ -1,24 +1,10 @@
 <?php
 require_once 'db.php';
+require_once 'get-list.php';
 $db = new MysqliDatabase();
 $conn = $db->connect();
 
+$getListHandler = new GetList($conn);
+$getListHandler->handle();
 
-$limit = empty($_GET['limit']) ? 100 : $_GET['limit'];
-$offset = $_GET['offset'] ?: 0;
-
-$sql = "SELECT * FROM links LIMIT ? OFFSET ?";
-$statement = $conn->prepare($sql);
-$statement->bind_param("ii", $limit, $offset);
-$statement->execute();
-$result = $statement->get_result();
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Source: " . $row["source"] . " - Short: " . $row["short"]. "\n";
-    }
-} else {
-    echo "0 results\n";
-}
 $conn->close();
