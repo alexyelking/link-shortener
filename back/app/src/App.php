b83b4e8cb397d00ae15482e6d892b4310e3066b9
Alexander <mistodrake@gmail.com>
2023-06-03 20:37:19 +0600

    Modal window

--- back/app/src/App.php
+++ back/app/src/App.php
@@ -13,12 +13,22 @@ class App
                 $getListHandler->handle();
                 break;
             case $_SERVER['REDIRECT_URL'] == '/link/create' && $_SERVER['REQUEST_METHOD'] == 'POST':
+                header("Access-Control-Allow-Origin: http://localhost:3000");
+                header("Access-Control-Allow-Methods: POST");
+                header("Access-Control-Request-Method: POST");
+                header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin");
                 $shortLinkHandler = new CreateShortLink($conn);
                 $shortLinkHandler->handle();
                 break;
-            case  strlen($_SERVER['REDIRECT_URL']) == 9 && $_SERVER['REQUEST_METHOD'] == 'GET':
+            case strlen($_SERVER['REDIRECT_URL']) == 9 && $_SERVER['REQUEST_METHOD'] == 'GET':
                 $tryRedirect = new Redirect($conn);
                 $tryRedirect->handle();
+                break;
+            case $_SERVER['REQUEST_METHOD'] == 'OPTIONS':
+                header("Access-Control-Allow-Origin: http://localhost:3000");
+                header("Access-Control-Allow-Methods: POST");
+                header("Access-Control-Request-Method: POST");
+                header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin");
                 break;
             default:
                 $notFoundHandler = new NotFound();
--- front/src/app.js
+++ front/src/app.js
@@ -9,10 +9,44 @@ form.addEventListener('submit', submitHandler)
 
 async function submitHandler(event) {
     event.preventDefault()
-    await axios
-        .post('http://0.0.0.0:8001/link/create', {
-            source: input.value,
-        })
-        .then(response => console.log(response))
-        .catch(error => console.log(error))
+    let res;
+    let formdata = new FormData()
+    formdata.append("source", input.value)
+    await axios.post("http://0.0.0.0:8001/link/create", formdata)
+        .then(response => createModal(response.data.data.link))
+        .catch(error => alert(error.response.data))
+
+    // let text = document.getElementById('copyLink')
+    // console.log(text)
+    // console.log(typeof text)
+    // text.select()
+    // document.execCommand("copy")
+
+    // input.value = ''
+}
+
+function createModal(shortLink) {
+    const modal = document.createElement('div')
+    modal.classList.add('modal')
+    modal.innerHTML = `
+        <h1>Your short link:</h1>
+        <hr>
+        <div class="modal-content">
+            <a href="${shortLink}" id="copyLink">
+                ${shortLink}
+            </a>
+        </div>
+    `
+
+    // shortLink.select()
+    // document.execCommand("copy")
+    // let text = document.getElementById("copyLink")
+    // console.log(text)
+    // text.addEventListener('click', getCopy)
+    // function getCopy() {
+    //     text.select()
+    //     document.execCommand("copy");
+    // }
+
+    mui.overlay('on', modal)
 }
