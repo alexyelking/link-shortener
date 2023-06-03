44e5f33939a0803fd25b00be101004266958ed3f
Alexander <mistodrake@gmail.com>
2023-06-03 23:16:30 +0600

    Copy link ready

--- front/src/app.js
+++ front/src/app.js
@@ -9,20 +9,11 @@ form.addEventListener('submit', submitHandler)
 
 async function submitHandler(event) {
     event.preventDefault()
-    let res;
     let formdata = new FormData()
     formdata.append("source", input.value)
     await axios.post("http://0.0.0.0:8001/link/create", formdata)
         .then(response => createModal(response.data.data.link))
         .catch(error => alert(error.response.data))
-
-    // let text = document.getElementById('copyLink')
-    // console.log(text)
-    // console.log(typeof text)
-    // text.select()
-    // document.execCommand("copy")
-
-    // input.value = ''
 }
 
 function createModal(shortLink) {
@@ -32,21 +23,14 @@ function createModal(shortLink) {
         <h1>Your short link:</h1>
         <hr>
         <div class="modal-content">
-            <a href="${shortLink}" id="copyLink">
+            <a href="${shortLink}" id="copyLink" class="copyLink">
                 ${shortLink}
             </a>
+            <br>
+            <button onclick="navigator.clipboard.writeText('${shortLink}')" class="mui-btn mui-btn--raised mui-btn--primary">Copy</button>
         </div>
     `
-
-    // shortLink.select()
-    // document.execCommand("copy")
-    // let text = document.getElementById("copyLink")
-    // console.log(text)
-    // text.addEventListener('click', getCopy)
-    // function getCopy() {
-    //     text.select()
-    //     document.execCommand("copy");
-    // }
-
     mui.overlay('on', modal)
 }
+
+//
