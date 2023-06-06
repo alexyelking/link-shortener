b01e7e2dd12ad7ce513952bacfc4361d0b8583ea
Alexander <mistodrake@gmail.com>
2023-06-06 23:56:01 +0600

    CreateShortLink refactoring

--- back/app/src/CreateShortLink.php
+++ back/app/src/CreateShortLink.php
@@ -22,38 +22,40 @@ class CreateShortLink
 
     public function handle()
     {
-        $ip = $_SERVER['SERVER_ADDR'];
-        $limit = 10;
-        if (!empty($_POST['source'])) {
-            if ((int)$this->redis->get($ip) < $limit) {
-                $source = $_POST['source'];
-
-                $link = $this->links->create($source);
-
-                $this->redis->incr($ip);
-                $this->redis->expire($ip, 60);
-
-                $shortUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $link->short;
-
-                $msg = new AMQPMessage('There was a reduction of some link.' . "%0A" . 'Source link: ' . urlencode($source) . "%0A" . 'Short link: ' . $shortUrl);
-                $this->channel->basic_publish($msg, '', 'cat-queue');
-
-                header('Content-Type: application/json; charset=utf-8');
-                echo json_encode([
-                    "message" => "Create successful",
-                    "data" => [
-                        "short" => $link->short,
-                        "link" => $shortUrl
-                    ]
-                ]);
-            } else {
-                http_response_code(429);
-                echo "Too many requests";
-            }
-
-        } else {
+        if (empty($_POST['source'])) {
             http_response_code(422);
             echo "Source link is required";
+            return;
         }
+
+        $ip = $_SERVER['SERVER_ADDR'];
+        $limit = 10;
+
+        if ((int)$this->redis->get($ip) >= $limit) {
+            http_response_code(429);
+            echo "Too many requests";
+            return;
+        } else {
+            $this->redis->incr($ip);
+            $this->redis->expire($ip, 60);
+        }
+
+        $source = $_POST['source'];
+
+        $link = $this->links->create($source);
+
+        $shortUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/' . $link->short;
+
+        $msg = new AMQPMessage('There was a reduction of some link.' . "%0A" . 'Source link: ' . urlencode($source) . "%0A" . 'Short link: ' . $shortUrl);
+        $this->channel->basic_publish($msg, '', 'cat-queue');
+
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode([
+            "message" => "Create successful",
+            "data" => [
+                "short" => $link->short,
+                "link" => $shortUrl
+            ]
+        ]);
     }
 }
