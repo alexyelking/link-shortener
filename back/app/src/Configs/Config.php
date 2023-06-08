3661ed2875ba993d57ef4a981a60be7b3c02e267
Alexander <mistodrake@gmail.com>
2023-06-08 14:03:39 +0600

    Config p2

--- back/app/src/Configs/Config.php
+++ back/app/src/Configs/Config.php
@@ -2,7 +2,35 @@
 
 namespace Shortener\Configs;
 
-class Config implements IAMQPConfig, ITelegramConfig, IRedisConfig, IDatabaseConfig
+class Config implements IDatabaseConfig
 {
-    // TODO
+    private $host;
+    private $username;
+    private $password;
+    private $name;
+
+    public function load()
+    {
+        //TODO
+    }
+
+    public function getHost()
+    {
+        $this->host = $_ENV['DB_HOST'];
+    }
+
+    public function getUsername()
+    {
+        $this->username = $_ENV['DB_USERNAME'];
+    }
+
+    public function getPassword()
+    {
+        $this->password = $_ENV['DB_PASSWORD'];
+    }
+
+    public function getName()
+    {
+        $this->name = $_ENV['DB_NAME'];
+    }
 }
